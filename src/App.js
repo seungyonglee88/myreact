0ddf2a581e27250b8674435650bda48d84fd94dc
seungyonglee88 <117351206+seungyonglee88@users.noreply.github.com>
2022-12-30 18:17:12 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import Counter from "./Counter";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello lsy</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+function Counter() {
+  return (
+    <div className="Counter">
+      <div>0</div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default Counter;
